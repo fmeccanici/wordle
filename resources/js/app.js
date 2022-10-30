756cf8cc3520d1aa9538b140b7a3220e3da47607
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-30 17:19:21 +0100

    ep 5 finished

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
-let grid = document.querySelector('#game');
+import game from './game';
 
-// the number of guesses (3)
-// the length of the word (3)
-
-let guessesAllowed = 4;
-let wordLength = 3;
+document.addEventListener('alpine:init', () => {
+     Alpine.data('game', () => game);
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,26 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>TryCat</title>
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
     <body>
-    <div id="game" x-data="{guessesAllowed : 4, wordLength : 3}">
-        <template x-for="row in Array.from({length : guessesAllowed})">
+    <div
+        id="game"
+        x-data="game"
+        @keyup.window="onKeyPress($event.key)">
+        <template x-for="row in board">
             <div class="row">
-                <template x-for="tile in Array.from({length : wordLength})">
-                    <div class="tile">
+                <template x-for="tile in row">
+                    <div class="tile" x-text="tile.letter">
 
                     </div>
                 </template>
             </div>
         </template>
-
     </div>
 
     </body>
+
 </html>
