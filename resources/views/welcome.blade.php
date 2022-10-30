f3f7ef64702bd165fe698f2651ccfae64e766c00
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-30 16:12:34 +0100

    ep 4 finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,25 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>TryCat</title>
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-    <div id="game">
-{{--        <div class="row">--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--            <div class="tile"></div>--}}
-{{--        </div>--}}
+    <div id="game" x-data="{guessesAllowed : 4, wordLength : 3}">
+        <template x-for="row in Array.from({length : guessesAllowed})">
+            <div class="row">
+                <template x-for="tile in Array.from({length : wordLength})">
+                    <div class="tile">
+
+                    </div>
+                </template>
+            </div>
+        </template>
+
     </div>
 
     </body>
