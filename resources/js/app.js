62263e2eb0901a80a152e9d425505df864fb1f55
fmeccanici <florismeccanici@mecctech-solutions.nl>
2022-10-30 16:11:46 +0100

    ep 3 finished

---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+let grid = document.querySelector('#game');
+
+// the number of guesses (3)
+// the length of the word (3)
+
+let guessesAllowed = 4;
+let wordLength = 3;
+
+let fragment = document.createDocumentFragment();
+
+let generateGrid = () => {
+    Array.from({ length: guessesAllowed}).forEach(() => {
+        let row = document.createElement('div');
+
+        row.classList.add('row');
+
+        Array.from({ length: wordLength}).forEach(() => {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+
+            row.appendChild(tile);
+        });
+
+        fragment.appendChild(row);
+    })
+
+    grid.appendChild(fragment);
+}
+
+// Init
+generateGrid();
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TryCat</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+    <div id="game">
+{{--        <div class="row">--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--        </div>--}}
+{{--        <div class="row">--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--        </div>--}}
+{{--        <div class="row">--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--            <div class="tile"></div>--}}
+{{--        </div>--}}
+    </div>
+
+    </body>
+</html>
